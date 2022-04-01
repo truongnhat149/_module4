aeab2ed4bae6edf66497e04eac1b8abf149d60d1
Truong <vjc.ichi.149@gmail.com>
2022-04-01 10:57:05 +0700

    data-biding

---
+++ data-biding/databiding-form/src/main/java/com/codegym/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.codegym.controller;
+
+import com.codegym.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("employee")
+public class EmployeeController {
+
+    //    @RequestMapping(value = "showForm", method = RequestMethod.GET)
+    @GetMapping("/showForm")
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    //    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    @PostMapping("/addEmployee")
+    public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        if(employee.getBalance()==0){
+            employee.setBalance(100);
+        }
+        model.addAttribute("balance", employee.getBalance());
+        return "employee/info";
+    }
+}
