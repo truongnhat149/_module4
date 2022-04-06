9df0d37f091e5612048ffb50290b1129e5081912
Truong <vjc.ichi.149@gmail.com>
2022-04-06 16:55:13 +0700

    validate

---
+++ JAMES/validate/input-validate/src/main/java/lpnt/cg/controller/UserController.java
@@ -0,0 +1,29 @@
+package lpnt.cg.controller;
+
+import lpnt.cg.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @GetMapping("/user")
+    public ModelAndView showForm() {
+        ModelAndView modelAndView = new ModelAndView("/index2");
+        modelAndView.addObject("user", new User("A",12));
+        return modelAndView;
+    }
+
+    @PostMapping("/validateUser")
+    public ModelAndView checkValidation(@Validated @ModelAttribute("user") User user, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            return new ModelAndView("/index2");
+        }
+        return new ModelAndView("/result");
+    }
+
+}
